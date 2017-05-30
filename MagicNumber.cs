b482fd2559901a81b49daad14975eaaee664c313
Vornikova-Murova <vornikovamasha@gmail.com>
2017-05-30 08:50:07 +0300

    Add files via upload

---
+++ MagicNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oval
+{
+    class Circle
+    {
+        public double r;
+        public double v;
+        const double PI_constant=3.14;///////РЕФАКТОРИНГ 1
+
+        public void area(double s)//тут можно заменить ПИ. замена магического числа символьной константой
+        {
+            r = Math.Sqrt(s/PI_constant);
+            //return r;               // рафакторинг 2. замена переменной вызовом метода
+        }
+        double Radius
+        {
+            get
+            {return r;}
+
+            set 
+            { r = value; }
+        }
+        public double volume(double r)
+        {
+            v = (4 / 3) * PI_constant * Radius*Radius*Radius; //тут опять же заменить ПИ. и воспользоваться вызовом метода радиуса 
+            return v;
+        }
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            c.area(-5);
+            Console.WriteLine("Area of circle is:"+c.r);
+            c.volume(1.32);
+            Console.WriteLine("Area of sphere is:" + c.v);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ReplaceTempWithAQuery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oval
+{
+    class Circle
+    {
+        public double r;
+        public double v;
+        const double PI_constant=3.14;
+       
+        public void area(double s)
+        {
+            r = Math.Sqrt(s/PI_constant);
+                          
+        }
+
+        double Radius
+        {
+            get
+            {return r;}
+
+            set 
+            { r = value; }
+        }
+        
+        public double volume(double r)
+        {
+            v = (4 / 3) * PI_constant * Radius*Radius*Radius; 
+            return v;
+        }
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            c.area(5.5);
+            Console.WriteLine("Area of circle is: "+c.r);
+            c.volume(1.32);
+            Console.WriteLine("Area of sphere is: " + c.v);
+            Console.ReadLine();
+
+        }
+    }
+}
